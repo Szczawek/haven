fcff5fe5c2041e9ad36987242a1180762f9f1788
Szczawik <szczawik.rozwoju@wp.pl>
2025-12-28 22:57:36 +0100

    temporary

--- api/roots/shop/addItem.go
+++ api/roots/shop/addItem.go
@@ -8,21 +8,23 @@ import (
 )
 
 type Item struct {
-	Name    string  `json:"name"`
-	Price   float64 `json:"price"`
-	Amount  uint16  `json:"amount"`
-	OwnerID uint16  `json:"ownerID"`
+	Name   string  `json:"name"`
+	Price  float64 `json:"price"`
+	Amount uint16  `json:"amount"`
 }
 
 func AddItem(res http.ResponseWriter, req *http.Request) {
-	defer req.Body.Close()
 	var pItem Item
 
 	cookie, errC := req.Cookie("session")
-	id := cookie.Value
+	if errC != nil {
+		http.Error(res, "cookies error", http.StatusBadRequest)
+		return
+	}
+	id, _ := strconv.ParseUint(cookie.Value, 10, 16)
 
-	if errC != nil || id != strconv.Itoa(int(pItem.OwnerID)) {
-		http.Error(res, "invalid data", http.StatusBadRequest)
+	if err := json.NewDecoder(req.Body).Decode(&pItem); err != nil {
+		http.Error(res, "json error", http.StatusInternalServerError)
 		return
 	}
 	if pItem.Price < 1.0 || pItem.Amount < 1 {
@@ -30,16 +32,14 @@ func AddItem(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	if err := json.NewDecoder(req.Body).Decode(&pItem); err != nil {
-		http.Error(res, "json error", http.StatusInternalServerError)
-		return
-	}
+	defer req.Body.Close()
 
-	const command string = "INSERT INTO shop(name,price,amout) VALUES(?,?,?)"
-	_, err := db.DB.Exec(command, pItem.Name, pItem.Price, pItem.Amount)
+	const command string = "INSERT INTO shop(name,price,amount,ownerID) VALUES(?,?,?,?)"
+	_, err := db.DB.Exec(command, pItem.Name, pItem.Price, pItem.Amount, id)
 	if err != nil {
 		http.Error(res, "Database error", http.StatusInternalServerError)
 		return
 	}
+
 	res.Write([]byte("Item was added!"))
 }
--- client/src/roots/shop/AddItemForm.jsx
+++ client/src/roots/shop/AddItemForm.jsx
@@ -1,12 +1,41 @@
 import {Link} from "react-router";
+import {useState} from "react";
+const stdData = {
+    name:"",
+    price:"",
+    amount:1,
+}
+
+const stdLoading = {
+    loading:false,
+    error:false,
+}
+
 export default function AddItemForm() {
-        
+    const [data, setData] = useState(stdData);
+    const [loading, setLoading] = useState(stdData);
+
+    function refresh(name,bool) {
+        setLoading(prev =>({...prev, [name]:bool}));
+    }
+
+    function rewrite(e) {
+        const {name,value} = e.target;
+        setData(prev => ({...prev,[name]:value}));
+    }
+    function disabled(e) {
+        console.log(e.target.value)
+        if(e.target.value.includes(".")) console.log(2);
+        rewrite(e);
+    }
     async function submit(e) {
         try {
+            refresh("loading",true);
             e.preventDefault();    
         } catch(err) {
+            refresh("error",true);
         } finally {
-
+            refresh("loading",false);
         }
     }
 
@@ -31,9 +60,11 @@ export default function AddItemForm() {
                 </label>
                 <label>
                     Amount
-                    <input id="inp-shop-a" placeholder="iteam amount..." required/>
+                    <input id="inp-shop-a" onChange={disabled} autoComplete="off" value={data.amount} min="1" max="1000000" type="number" name="amount" placeholder="iteam amount..." required/>
+        i
                 </label>
                 <button className="sb" type="submit">Submit</button>
             </form>
         </div>
 }
+
